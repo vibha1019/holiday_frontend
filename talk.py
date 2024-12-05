cba32bc68755a8180b1b4c97ce066c1b2055bc17
Frogpants <spencerjl0309@gmail.com>
2024-12-04 22:15:11 -0800

    communication between repositories file

---
+++ talk.py
@@ -0,0 +1,11 @@
+import sys
+
+class frontend:
+    def __init__(self):
+        pass
+    def send(self,path):
+        with open(path, 'w') as file:
+            file.write(file)
+    def fetch(self):
+        with open("/socialmedia_backend_fork/talk.py", 'r') as file:
+            return file.read()
