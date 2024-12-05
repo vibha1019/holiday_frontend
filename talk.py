ffaf8832b19ca67c38dab9ef5275c0fe8ae19044
Frogpants <spencerjl0309@gmail.com>
2024-12-05 10:12:41 -0800

    upd to talk.py

--- talk.py
+++ talk.py
@@ -3,9 +3,9 @@ import sys
 class frontend:
     def __init__(self):
         pass
-    def send(self,path):
+    def send(self,data,path):
         with open(path, 'w') as file:
-            file.write(file)
+            file.write(data)
     def fetch(self):
         with open("/socialmedia_backend_fork/talk.py", 'r') as file:
             return file.read()
