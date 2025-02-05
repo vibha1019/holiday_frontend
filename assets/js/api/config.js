22a210ba5019f1d4e5cdb310923f07434da84f63
vibha1019 <vibhamandayam08@gmail.com>
2025-02-05 10:52:37 -0800

    update config.js

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -4,7 +4,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
         pythonURI = "http://127.0.0.1:8206";
 } else {
-        pythonURI =  "https://flocker.nighthawkcodingsociety.com";
+        pythonURI =  "https://holiday.stu.nighthawkcodingsociety.com";
 }
 
 // Log the value of pythonURI to the console
